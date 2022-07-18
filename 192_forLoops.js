be4d1db409103e5edf84c1bf849319e30f8fa17a
Eric <erictaynguyen@gmail.com>
2022-07-18 16:52:19 -0700

    for loop practice

---
+++ 192_forLoops.js
@@ -0,0 +1,16 @@
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i)
+}
+
+// 0, 2, 4, etc. 
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i)
+}
+
+// 2, 4, 6, 8
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+// 100, 90, 80 etc
