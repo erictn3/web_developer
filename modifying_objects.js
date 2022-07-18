61bf72c5e8495171d9344610ac4761cb42055a61
Eric <erictaynguyen@gmail.com>
2022-07-18 14:51:39 -0700

    modifying objects capabilities

---
+++ modifying_objects.js
@@ -0,0 +1,12 @@
+const midterms = { danielle: 96, thomas: 78 };
+
+midterms.thomas = 79;
+// you can change the insides of items in objects
+
+midterms.thomas = 'C+'
+midterms['danielle'] = 'A';
+
+midterms.ezra = 'B+'
+
+midterms['Antonio'] = 'C+';
+// with quotes, they are the key of the key value pair
