57d4bfca04f102c7110cf4ddf78c172da96d5be8
Eric <erictaynguyen@gmail.com>
2022-07-19 16:52:21 -0700

    for loop additional practice

--- 199.js
+++ 199.js
@@ -1,25 +1,33 @@
-const subreddits = ["cringe", "books", "chickens", "funny", "pics"];
+// const subreddits = ["cringe", "books", "chickens", "funny", "pics"];
 
-for (let i = 0; i < subreddits.length; i++) {
-    console.log(`Visit reddit.com/r/${subreddits[i]}`);
-}
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
 
-for (let item of subreddits) {
-    console.log(item);
-}
+// for (let item of subreddits) {
+//     console.log(item);
+// }
 
-for (let i of subreddits) {
-    console.log(i);
-}
+// for (let i of subreddits) {
+//     console.log(i);
+// }
 
-const seatingChart = [
-    ['kristen', 'erik', 'dave'],
-    ['chris', 'derick', 'llave'],
-    ['main', 'ike', 'mave'],
-]
+// const seatingChart = [
+//     ['kristen', 'erik', 'dave'],
+//     ['chris', 'derick', 'llave'],
+//     ['main', 'ike', 'mave'],
+// ]
 
-for (let row of seatingChart) {
-    for (let student of row) {
-        console.log(student);
-    }
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let num of numbers) {
+    console.log(num * num);
 }
