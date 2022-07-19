6483ab7517f05f186c3b69bdd0e3f4854ea6820b
Eric <erictaynguyen@gmail.com>
2022-07-19 16:46:34 -0700

    additional practice for loops to for of practice

--- 199.js
+++ 199.js
@@ -3,3 +3,23 @@ const subreddits = ["cringe", "books", "chickens", "funny", "pics"];
 for (let i = 0; i < subreddits.length; i++) {
     console.log(`Visit reddit.com/r/${subreddits[i]}`);
 }
+
+for (let item of subreddits) {
+    console.log(item);
+}
+
+for (let i of subreddits) {
+    console.log(i);
+}
+
+const seatingChart = [
+    ['kristen', 'erik', 'dave'],
+    ['chris', 'derick', 'llave'],
+    ['main', 'ike', 'mave'],
+]
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
