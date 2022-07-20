e7d8a9c08d7957cbed1bea86f1fd7f4654ad5c6a
Eric <erictaynguyen@gmail.com>
2022-07-19 17:45:46 -0700

    edge cases

--- 07_19_22/202.js
+++ 07_19_22/202.js
@@ -14,8 +14,13 @@ while (input !== 'quit' && input !== 'q') {
         todos.push(newTodo);
         console.log(`${newTodo} added to the list`);
     } else if (input === 'delete') {
-        const index = prompt('Hrm, what index do you want to delete?');
-        todos.splice(index, 1)
+        const index = parseInt(prompt('Hrm, what index do you want to delete?'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1)
+            console.log(`Okay, deleted ${deleted[0]}`)
+        } else {
+            console.log('Unknown Index');
+        }
     }
     input = prompt('what would you like to do?')
 }
