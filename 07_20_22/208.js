3027a506d27f4e751260bebed301423d6d18fb22
Eric <erictaynguyen@gmail.com>
2022-07-20 17:22:08 -0700

    weekDay function exercise

--- 07_20_22/208.js
+++ 07_20_22/208.js
@@ -38,3 +38,22 @@ function sumArray(array) {
     }
     return sum;
 }
+
+
+const weekDays = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday',
+}
+
+const returnDay = (day) => {
+    if (day < 1 || day > 7) {
+        return null;
+    }
+    return (weekDays[day])
+
+}
