16dd2e33cf7d942690c83001d24e0335f598aa81
Eric <erictaynguyen@gmail.com>
2022-07-25 18:13:12 -0700

    defining methods

---
+++ 7_25_22/216.js
@@ -0,0 +1,14 @@
+// a method is a function that has been placed as a property on an object
+// every method is a funciton but not every function is a method
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
+
+// every methods are simply functions added as properties on objects so we access them
