364d55a16ca30cfe8f00321342f722bb7d1be5c2
Eric <erictaynguyen@gmail.com>
2022-07-26 15:55:00 -0700

    understanding the this keyword

---
+++ 07_26_22/217.js
@@ -0,0 +1,25 @@
+// const cat = {
+//     name: 'Blue Steele',
+//     color: 'grey',
+//     breed: 'scottish fold',
+//     meow(){
+//         console.log('meow meow meow')
+//     }
+// }
+
+const cat = {
+    name: 'Blue Steele',
+    color: 'grey',
+    breed: 'scottish fold',
+    meow() {
+        console.log(`${this.name} says meow`);
+    }
+}
+
+
+
+// key word THIS refers to object
+
+const meow2 = cat.meow;
+
+// 
