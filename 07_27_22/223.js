5c2c4fb66deff9257675a1e3947c8c7d00c995d8
Eric <erictaynguyen@gmail.com>
2022-07-27 14:46:48 -0700

    implicit returns without arrow functions

--- 07_27_22/223.js
+++ 07_27_22/223.js
@@ -2,18 +2,19 @@
 //     return x + y;
 // }
 
-const add = (x, y) => {
-    return x + y
-}
+// const add = (x, y) => {
+//     return x + y
+// }
 
-const square = (x) => {
-    return x ** 2
-}
+// const square = (x) => {
+//     return x ** 2
+// }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
 
-const greet = (name) => {
-    return `Hey ${name}!`
-}
+// const greet = (name) => {
+//     return `Hey ${name}!`
+// }
+
---
+++ 07_27_22/224.js
@@ -0,0 +1,14 @@
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+
+const subtract = (a, b) => (
+    a - b
+)
+
+const add = (a, b) => a + b
+
+const divide = (a, b) => {
+    return a / b
+}
