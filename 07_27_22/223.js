c4afc9e294d0bc48144fcd2c4691a2aa95e7a7c5
Eric <erictaynguyen@gmail.com>
2022-07-27 14:39:04 -0700

    practicing arrow function

---
+++ 07_27_22/223.js
@@ -0,0 +1,15 @@
+// const add = function(x,y) {
+//     return x + y;
+// }
+
+const add = (x, y) => {
+    return x + y
+}
+
+const square = (x) => {
+    return x ** 2
+}
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
