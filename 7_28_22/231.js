6da536a9082830af3aa11305dffba40045b47005
Eric <erictaynguyen@gmail.com>
2022-07-28 15:03:43 -0700

    default params and edge cases for them

--- 7_28_22/231.js
+++ 7_28_22/231.js
@@ -1,3 +1,10 @@
+// function rollDie(numSides) {
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
 function rollDie(numSides) {
+    if (numSides === undefined) {
+        numSides = 6;
+    }
     return Math.floor(Math.random() * numSides) + 1
 }
