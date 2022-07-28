e6665c87399c74c7f711bce2d2e4085a06344c79
Eric <erictaynguyen@gmail.com>
2022-07-28 15:17:28 -0700

    additional default params

--- 7_28_22/231.js
+++ 7_28_22/231.js
@@ -2,9 +2,19 @@
 //     return Math.floor(Math.random() * numSides) + 1
 // }
 
-function rollDie(numSides) {
-    if (numSides === undefined) {
-        numSides = 6;
-    }
+// function rollDie(numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+function rollDie(numSides = 6) {
     return Math.floor(Math.random() * numSides) + 1
+}
+
+// num sides is set as a default
+
+function greet(msg = 'Hello There', person = 'Random Person', punc = '!') {
+    console.log(`${msg}, ${person} ${punc}`);
 }
