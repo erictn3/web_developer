63b019501d523f3f8b9d87054a18abe16a31fd0f
Eric <erictaynguyen@gmail.com>
2022-08-01 18:52:20 -0700

    get element by classname

---
+++ 8_1_22/244.js
@@ -0,0 +1,11 @@
+// const allImages = document.getElementsByTagName('img');
+
+// for(let img of allImages){
+//     img.src = ''
+// }
+
+// document.getElementsByClassName('square'); 
+const squareImages = document.getElementsByClassName('square')
+for (let img of squareImages) {
+    img.src = ''
+}
