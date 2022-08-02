8b3a964f09f2c1c99f5390fa2b9f8c15aeaaa6be
Eric <erictaynguyen@gmail.com>
2022-08-02 16:31:16 -0700

    attribute practice

---
+++ 8_2_22/exercise54.js
@@ -0,0 +1,20 @@
+// Manipulating Attributes Practice
+// Let's get some practice working with DOM element attributes. I've provided you with some very basic markup.  Please select the image element and:
+
+// change its source to this url: https://devsprouthosting.com/images/chicken.jpg
+
+// change its alt text to be "chicken"
+
+// Egg and Chicken
+// <h4>Which Came First?</h4>
+// <img src="https://devsprouthosting.com/images/egg.jpg" width="200px">
+// <!-- image source: unsplash.com -->
+
+
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const image = document.querySelector('img');
+
+image.src = 'https://devsprouthosting.com/images/chicken.jpg'
+
+image.alt = 'chicken';
