7b06bb6275489dde7ca4e7c0c514f6689e5128df
Eric <erictaynguyen@gmail.com>
2022-08-02 16:12:32 -0700

    attributes

---
+++ 8_2_22/247.js
@@ -0,0 +1,13 @@
+document.querySelector('#banner').id = 'whoops';
+document.querySelector('#banner').src = 'whoops';
+
+// types of attributes  src, href, title
+
+const firstLink = document.querySelector('a');
+
+firstLink.href;
+firstLink.getAttribute('href');
+firstLink.getAttribute('class');
+
+document.querySelector('input[type="text"]')
+// css attribute selector
