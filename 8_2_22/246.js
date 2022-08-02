9a784eb2233bafe8bce2d0be2438328dcdeaf90a
Eric <erictaynguyen@gmail.com>
2022-08-02 15:44:11 -0700

    innerHTML, innerContext, innerText

---
+++ 8_2_22/246.js
@@ -0,0 +1,22 @@
+document.querySelector('h1');
+
+document.querySelector('p').innerText;
+
+document.querySelector('p').innerText = 'lololol'
+// 
+
+document.querySelector('p').textContent;
+// returns every piece of element for textContent including bold/underline/etc.
+
+// returns every piece of text
+
+// const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks){
+//     link.innerText = 'I AM A LINK!!!'
+// }
+
+document.querySelector('h1').innerText = '<i>asdfasdf<i>'
+
+document.querySelector('h1').innerHTML = '<i>asdfasdf<i>'
+// we see the html elements included with the text
