0c3f286f8143107d94107e3c7a4784ea3b41cd42
Eric <erictaynguyen@gmail.com>
2022-08-03 11:10:12 -0700

    classList

---
+++ 08_3_22/249.js
@@ -0,0 +1,19 @@
+// const h2 = document.querySelector('h2');
+
+// h2.setAttribute('class');
+
+// h2.setAttribute('class', 'purple');
+
+// h2.setAttribute('class', 'border');
+
+// let currentClasses = h2.getAttribute('class');
+
+// h2.setAttribute('class', `${currentClasses} purple`)
+
+h2.classList.add('purple')
+h2.classList.add('border');
+
+h2.classList.remove('border')
+h2.classList.contains('border');
+
+h2.classList.toggle('purple');
