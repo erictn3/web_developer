8ba38e0d13d676d510362e88f89e389acede9c70
Eric <erictaynguyen@gmail.com>
2022-08-05 16:29:45 -0700

    add event listener more examples

--- 8_4/257/app.js
+++ 8_4/257/app.js
@@ -18,3 +18,21 @@ btn3.addEventListener('click', function () {
 
 })
 
+function twist() {
+    console.log('TWIST')
+}
+
+function twist() {
+    console.log('SHOUT')
+}
+
+const tasButton = document.querySelector('tas');
+
+tasButton.onclick = twist;
+tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist)
+
+tasButton.addEventListener('click', shout)
+
+
