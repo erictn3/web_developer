3ba8b8b293fc9b2149f3da5028551890700b14c2
Eric <erictaynguyen@gmail.com>
2022-08-09 15:09:47 -0700

    preventdefault and form events practice

--- 8_5/262/app.js
+++ 8_5/262/app.js
@@ -1,6 +1,15 @@
-const form = document.querySelector('#shelterForm')
+const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats');
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('hi')
+    const catName = input.value;
+
+    const newLI = document.createElement('LI');
+    newLI.innerText = catName;
+    list.append(newLI);
+    input.value = '';
 });
+
 
