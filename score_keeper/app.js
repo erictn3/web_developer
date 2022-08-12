47088105bd428123eb6a0bc8d9d6d337b189c4b6
Eric <erictaynguyen@gmail.com>
2022-08-11 18:25:43 -0700

    player score game part 2 of 4

--- score_keeper/app.js
+++ score_keeper/app.js
@@ -4,8 +4,27 @@ const p1Score = document.querySelector('#p1Score');
 const p2Score = document.querySelector('#p2Score');
 
 let p1Count = 0;
+let p2Count = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
-    p1Count += 1;
-    p1Score.innerHTML = p1Count;
+    if (!isGameOver) {
+        p1Count += 1;
+        if (p1Count === winningScore) {
+            isGameOver = true;
+        }
+        p1Score.textContent = p1Count;
+    }
 })
+
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Count += 1;
+        if (p2Count === winningScore) {
+            isGameOver = true;
+        }
+        p2Score.textContent = p2Count;
+    }
+})
+
