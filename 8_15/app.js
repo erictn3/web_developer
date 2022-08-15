e049a317709dacae8879f97f2ea30ee7c2006bc9
Eric <erictaynguyen@gmail.com>
2022-08-15 14:51:36 -0700

    javascript to browser functions

---
+++ 8_15/app.js
@@ -0,0 +1,10 @@
+console.log('sending request to server')
+setTimeout(() => {
+    console.log(
+        'data from server'
+    )
+}, 3000);
+
+console.log('this sends first');
+
+// browser does the work with their own browser apis
