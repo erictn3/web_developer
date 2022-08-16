22890a50a1158e5f3230d839a4cf110b10038e18
Eric <erictaynguyen@gmail.com>
2022-08-16 16:24:11 -0700

    fakeRequest using Promises

---
+++ 8-16/promise/app.js
@@ -0,0 +1,33 @@
+// THE PROMISE VERSION 
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+fakeRequestPromise('yelp.com/api/coffee/page1')
+    .then(() => {
+        console.log('worked page 1')
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                console.log('worked page 2')
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log('worked page 3')
+                    })
+            })
+            .catch(() => {
+                console.log('oh no, error page 2')
+            })
+    })
+    .catch(() => {
+        console.log('promise rejected')
+        console.log('oh no, error page 1')
+    })
