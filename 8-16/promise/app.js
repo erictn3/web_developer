6f684e45be918427f1fbffb60c1c9bdb1eb6ea58
Eric <erictaynguyen@gmail.com>
2022-08-16 16:30:58 -0700

    magic of promises part 1

--- 8-16/promise/app.js
+++ 8-16/promise/app.js
@@ -12,22 +12,38 @@ const fakeRequestPromise = (url) => {
     })
 }
 
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then(() => {
+//         console.log('worked page 1')
+//         fakeRequestPromise('yelp.com/api/coffee/page2')
+//             .then(() => {
+//                 console.log('worked page 2')
+//                 fakeRequestPromise('yelp.com/api/coffee/page3')
+//                     .then(() => {
+//                         console.log('worked page 3')
+//                     })
+//             })
+//             .catch(() => {
+//                 console.log('oh no, error page 2')
+//             })
+//     })
+//     .catch(() => {
+//         console.log('promise rejected')
+//         console.log('oh no, error page 1')
+//     })
+
 fakeRequestPromise('yelp.com/api/coffee/page1')
     .then(() => {
-        console.log('worked page 1')
-        fakeRequestPromise('yelp.com/api/coffee/page2')
-            .then(() => {
-                console.log('worked page 2')
-                fakeRequestPromise('yelp.com/api/coffee/page3')
-                    .then(() => {
-                        console.log('worked page 3')
-                    })
-            })
-            .catch(() => {
-                console.log('oh no, error page 2')
-            })
+        console.log('it worked! page 1')
+        return fakeRequestPromise('yelp.com/api/coffee/page2')
+    })
+    .then(() => {
+        console.log('it worked! page 2')
+        return fakeRequestPromise('yelp.com/api/coffee/page3')
+    })
+    .then(() => {
+        console.log('it worked! page 3')
     })
     .catch(() => {
-        console.log('promise rejected')
-        console.log('oh no, error page 1')
+        console.log('oh no, request failed')
     })
