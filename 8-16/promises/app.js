de811de96bd5405e0190826fa7667dbf8c2054ed
Eric <erictaynguyen@gmail.com>
2022-08-16 16:49:24 -0700

    create own promises part 2

--- 8-16/promises/app.js
+++ 8-16/promises/app.js
@@ -1,12 +1,21 @@
 const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
+        const rand = Math.random();
+
         setTimeout(() => {
-            resolve();
+            if (rand < 0.7) {
+                resolve('your fake data here');
+            }
+            reject('request error');
         }, 1000);
     })
 }
 
 fakeRequest('/dogs/1')
-    .then(() => {
+    .then((data) => {
         console.log('done with request')
+        console.log('data is:', data)
+    })
+    .catch((err) => {
+        console.log('oh no!', err)
     })
