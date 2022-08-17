66691e9bbf32493218ad59ae1cfc53218a3d08d7
Eric <erictaynguyen@gmail.com>
2022-08-17 16:41:28 -0700

    async practice

--- 8-17/1/app.js
+++ 8-17/1/app.js
@@ -2,16 +2,28 @@
 
 // }
 
-const sing = async () => {
-    throw 'oh no problem'
-    return 'LA LA LA LA'
+// const sing = async () => {
+//     throw 'oh no problem'
+//     return 'LA LA LA LA'
+// }
+
+// sing()
+//     .then((data) => {
+//         console.log('promised resolved with', data)
+//     })
+//     .catch(err => {
+//         console.log('Oh no, promise rejected')
+//         console.log(err)
+//     })
+
+const login = async (username, password) => {
+    if (!username || !password) throw 'missing credentials'
+    if (password === 'corgifeet') return 'Welcome'
+    throw 'invalid password'
 }
 
-sing()
-    .then((data) => {
-        console.log('promised resolved with', data)
-    })
-    .catch(err => {
-        console.log('Oh no, promise rejected')
-        console.log(err)
+login('asdfdf')
+    .then(msg => {
+        console.log('logged in')
+        console.log(msg)
     })
