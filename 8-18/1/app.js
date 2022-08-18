29e17de8532b2459e071bb61dfc9dee5d998f248
Eric <erictaynguyen@gmail.com>
2022-08-18 10:43:58 -0700

    await keyword

---
+++ 8-18/1/app.js
@@ -0,0 +1,20 @@
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    })
+}
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('purple', 1000)
+}
