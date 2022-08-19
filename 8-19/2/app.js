be9f4abdf66828bb5149f49afd23cbd7ae58e03d
Eric <erictaynguyen@gmail.com>
2022-08-19 15:30:30 -0700

    fetch API

--- 8-19/2/app.js
+++ 8-19/2/app.js
@@ -1,7 +1,18 @@
 fetch('https://swapi.dev/api/people/1')
     .then(res => {
         console.log('resolved', res)
-        res.json().then((data) => console.log('json done', data))
+        return res.json()
+    })
+    .then((data) => {
+        console.log('json done', data)
+        return fetch('https://swapi.dev/api/people/2')
+    })
+    .then(res => {
+        console.log('second response', res);
+        return res.json()
+    })
+    .then((data) => {
+        console.log('json 2 done', data)
     })
     .catch((e) => {
         console.log('error', e)
