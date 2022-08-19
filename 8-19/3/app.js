090b1cd1b228882e444c3111aca6cf2509a3cc21
Eric <erictaynguyen@gmail.com>
2022-08-19 16:26:26 -0700

    DOM axios manipulation

--- 8-19/3/app.js
+++ 8-19/3/app.js
@@ -6,21 +6,27 @@
 //         console.log('error', e)
 //     })
 
-const getStarWarsPerson = async (id) => {
-    try {
-        const res = await axios.get(`https://swapi.dev/api/people/${id}`);
-        console.log(res.data);
-    } catch (e) {
-        console.log(e)
-    }
-}
-getStarWarsPerson(4);
+// const getStarWarsPerson = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+// getStarWarsPerson(4);
+
+const jokes = document.querySelector('#joke')
 
 const dadJoke = async () => {
     const config = { headers: { Accept: 'application/json' } }
     try {
         const res = await axios.get('https://icanhazdadjoke.com/', config);
-        console.log(res.data);
+        console.log(res.data.joke);
+        const newLI = document.createElement('LI');
+
+        newLI.append(res.data.joke);
+        jokes.append(newLI);
     } catch (e) {
         console.log(e);
     }
