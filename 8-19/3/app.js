f1e8132de933e807fa54ce2a26bce98519af638c
Eric <erictaynguyen@gmail.com>
2022-08-19 15:50:24 -0700

    axios better than fetch practice

--- 8-19/3/app.js
+++ 8-19/3/app.js
@@ -1,4 +1,13 @@
-axios.get('https://swapi.dev/api/people/1')
-    .then((res) => {
-        console.log('response', res)
-    })
+// axios.get('https://swapi.dev/api/people/1')
+//     .then((res) => {
+//         console.log('response', res)
+//     })
+//     .catch((e) => {
+//         console.log('error', e)
+//     })
+
+const getStarWarsPerson = async (id) => {
+    const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+    console.log(res.data);
+}
+getStarWarsPerson(4);
