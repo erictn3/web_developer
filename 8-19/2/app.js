04f917ffc3dfb4b0cb0445eae98713e8a479588e
Eric <erictaynguyen@gmail.com>
2022-08-19 15:43:13 -0700

    fetch api part 2

--- 8-19/2/app.js
+++ 8-19/2/app.js
@@ -1,19 +1,35 @@
-fetch('https://swapi.dev/api/people/1')
-    .then(res => {
-        console.log('resolved', res)
-        return res.json()
-    })
-    .then((data) => {
-        console.log('json done', data)
-        return fetch('https://swapi.dev/api/people/2')
-    })
-    .then(res => {
-        console.log('second response', res);
-        return res.json()
-    })
-    .then((data) => {
-        console.log('json 2 done', data)
-    })
-    .catch((e) => {
+// fetch('https://swapi.dev/api/people/1')
+//     .then(res => {
+//         console.log('resolved', res)
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log('json done', data)
+//         return fetch('https://swapi.dev/api/people/2')
+//     })
+//     .then(res => {
+//         console.log('second response', res);
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log('json 2 done', data)
+//     })
+//     .catch((e) => {
+//         console.log('error', e)
+//     })
+
+
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch('https://swapi.dev/api/peereople/1')
+        const data = await res.json();
+        console.log(data);
+        const res2 = await fetch('https://swapi.dev/api/people/2')
+        const data2 = await res2.json();
+        console.log(data2);
+    } catch (e) {
         console.log('error', e)
-    })
+    }
+}
+
+loadStarWarsPeople();
