f7067ec7cfc7d9d738e6c4a5b64dec822a89f282
Eric <erictaynguyen@gmail.com>
2022-08-19 15:12:00 -0700

    making XHR

---
+++ 8-19/app.js
@@ -0,0 +1,17 @@
+// https://swapi.dev/api/people/5
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log('it loaded')
+    const data = JSON.parse(this.responseText);
+    console.log(data)
+}
+
+req.onerror = function () {
+    console.log('error!')
+    console.log(this)
+}
+
+req.open('GET', 'https://swapi.dev/api/people/5');
+req.send();
