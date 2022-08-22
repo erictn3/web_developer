f5e0de0497aed3b8fac58646dcbcdb2827c68baa
Eric <erictaynguyen@gmail.com>
2022-08-22 16:56:50 -0700

    tv show app part 2

--- 8-22/app.js
+++ 8-22/app.js
@@ -1,6 +1,13 @@
 const form = document.querySelector('#searchForm');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    console.dir(form)
+    const searchTerm = form.elements.query.value;
+
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+
+
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img);
 })
