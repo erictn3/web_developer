c365bc638284fbd8933aa52fd5289ae5e9698aaf
Eric <erictaynguyen@gmail.com>
2022-08-23 14:58:46 -0700

    search app final part

--- 8-22/app.js
+++ 8-22/app.js
@@ -3,11 +3,19 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm, isFunny: 'colt' } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
 
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    displayImages(res.data);
+    form.elements.query.value = '';
+});
 
-
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img);
-})
+const displayImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
