7eac6b9c31c83366f5007db24e57d2a1d80747b2
Eric <erictaynguyen@gmail.com>
2022-08-24 16:06:24 -0700

    fs writefile sync mkdir

--- 8-24/1/boilerplate.js
+++ 8-24/1/boilerplate.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
-console.log(fs)
+const folderName = process.argv[2] || 'Project'
 
 // fs.mkdir('Dogs', { recursive: true }, (err) => {
 //     console.log('in the callback')
 //     if (err) throw err;
 // });
 
-fs.mkdirSync('cats');
+fs.mkdirSync(folderName);
+fs.writeFileSync(`${folderName}/index.html`, '')
+fs.writeFileSync(`${folderName}/styles.css`, '')
+fs.writeFileSync(`${folderName}/app.js`, '')
 
-console.log('i come after mkdir in the file')
+// console.log('i come after mkdir in the file') 
