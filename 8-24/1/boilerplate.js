7f9b64493d85b30a5e6b91edf8bfb159f95e043b
Eric <erictaynguyen@gmail.com>
2022-08-24 16:08:09 -0700

    try catch revision to fs module

--- 8-24/1/boilerplate.js
+++ 8-24/1/boilerplate.js
@@ -6,9 +6,15 @@ const folderName = process.argv[2] || 'Project'
 //     if (err) throw err;
 // });
 
-fs.mkdirSync(folderName);
-fs.writeFileSync(`${folderName}/index.html`, '')
-fs.writeFileSync(`${folderName}/styles.css`, '')
-fs.writeFileSync(`${folderName}/app.js`, '')
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`, '')
+    fs.writeFileSync(`${folderName}/styles.css`, '')
+    fs.writeFileSync(`${folderName}/app.js`, '')
+} catch (e) {
+    console.log('something went wrong')
+    console.log(e);
+}
+
 
 // console.log('i come after mkdir in the file') 
