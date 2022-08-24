21c4105aca7a6af98d723e04ac780f4f567afa42
Eric <erictaynguyen@gmail.com>
2022-08-24 15:56:52 -0700

    make synchronous

--- 8-24/1/boilerplate.js
+++ 8-24/1/boilerplate.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 console.log(fs)
 
-fs.mkdir('Dogs', { recursive: true }, (err) => {
-    console.log('in the callback')
-    if (err) throw err;
-});
+// fs.mkdir('Dogs', { recursive: true }, (err) => {
+//     console.log('in the callback')
+//     if (err) throw err;
+// });
+
+fs.mkdirSync('cats');
 
 console.log('i come after mkdir in the file')
