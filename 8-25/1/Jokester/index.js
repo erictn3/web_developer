2edc2239df1711a09bf7af8983eafa4be7243005
Eric <erictaynguyen@gmail.com>
2022-08-29 15:17:36 -0700

    figlet node practice

--- 8-25/1/Jokester/index.js
+++ 8-25/1/Jokester/index.js
@@ -2,9 +2,17 @@ const jokes = require('give-me-a-joke');
 
 const colors = require('colors');
 
+const cowsay = require('cowsay');
+
 
 
 jokes.getRandomDadJoke(function (joke) {
     console.log(joke.rainbow);
 })
 
+// npm install for use in one directory and cannot require from outside
+// locally to one directory
+
+// package.json in every node app
+// meta data about the project
+// reliant on the dependencies
