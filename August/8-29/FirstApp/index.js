cb7b34f87da329edae26289f2978830483b2db91
Eric <erictaynguyen@gmail.com>
2022-08-29 16:28:34 -0700

    working on app.use functionality

--- August/8-29/FirstApp/index.js
+++ August/8-29/FirstApp/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// app.use - every time a new request hits our server we receive it and now we display that response
+app.use(() => {
+    console.log("We got a new request")
+})
+
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
 })
