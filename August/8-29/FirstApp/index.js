c009063faa0832b7e45c78183a5eec2d6b069a27
Eric <erictaynguyen@gmail.com>
2022-08-30 15:14:07 -0700

    app.get requests

--- August/8-29/FirstApp/index.js
+++ August/8-29/FirstApp/index.js
@@ -3,12 +3,32 @@ const express = require('express');
 const app = express();
 
 // app.use - every time a new request hits our server we receive it and now we display that response
-app.use((req, res) => {
-    console.log("We got a new request")
-    // res.send('hello, we got your request, this is a response');
-    res.send({ color: 'red' })
+// app.use((req, res) => {
+//     console.log("We got a new request")
+//     // res.send('hello, we got your request, this is a response');
+//     res.send({ color: 'red' })
+// })
+
+// http request get only one response
+
+app.get('/', (req, res) => {
+    res.send('this is the home page');
 })
 
+app.get('/cats', (req, res) => {
+
+    res.send('meow')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('woof');
+})
+
+// request is an object created by express based upon the incoming HTTP request
+
+// /cats => 'meow'
+// /dogs => 'woof'
+// '/' 
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
