87490d0276d4fd7cb7d332c62f17ad9475c5feed
Eric <erictaynguyen@gmail.com>
2022-09-02 13:53:37 -0700

    passing data to templates final part

--- August/8-31/index.js
+++ August/8-31/index.js
@@ -11,9 +11,15 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit })
+})
+
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', { rand: num })
+    // does not matter what you name your key/object
 })
 
 app.listen(3000, () => {
