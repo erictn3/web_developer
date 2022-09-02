b6eb6e4c9245d6754475fbf7277a7a48971116f9
Eric <erictaynguyen@gmail.com>
2022-09-02 15:46:12 -0700

    server static assets in express

--- August/8-31/index.js
+++ August/8-31/index.js
@@ -1,10 +1,12 @@
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const redditData = require('./data.json')
 console.log(redditData)
+
+app.use(express.static('public'))
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
