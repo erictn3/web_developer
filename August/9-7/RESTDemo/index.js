204db5c969ba8b4de581742d42ed4e3749cba556
Eric <erictaynguyen@gmail.com>
2022-09-07 11:55:21 -0700

    parsing request body

--- August/9-7/RESTDemo/index.js
+++ August/9-7/RESTDemo/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response')
 })
 
 app.post('/tacos', (req, res) => {
-    res.send('POST /tacos response')
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat}`)
 })
 
 app.listen(3000, () => {
