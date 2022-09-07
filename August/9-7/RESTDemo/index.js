b8b68bc758912b1bd5c9bfdca13216635f785462
Eric <erictaynguyen@gmail.com>
2022-09-07 15:42:55 -0700

    CRUD functionality blueprint

--- August/9-7/RESTDemo/index.js
+++ August/9-7/RESTDemo/index.js
@@ -1,5 +1,7 @@
+const { text } = require('express');
 const express = require('express');
 const app = express();
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -10,9 +12,28 @@ app.get('/tacos', (req, res) => {
 
 app.post('/tacos', (req, res) => {
     const { meat, qty } = req.body;
-    res.send(`OK, here are your ${qty} ${meat}`)
+    res.send(`OK, here are your ${qty}`)
 })
 
 app.listen(3000, () => {
     console.log('ON PORT 3000!')
 })
+
+
+username
+text
+
+bob - hello!
+
+GET / allcomments
+GET / showmeallcommentsnow
+
+POST / newcomment
+POST / makecomment
+
+GET / comments - list all comments
+POST / comments - create a new comment
+GET / comments /: id - Get one comment(using ID)
+
+PATCH / comments /: id - update one comment
+DELETE / comments /: id - destroy one comment
