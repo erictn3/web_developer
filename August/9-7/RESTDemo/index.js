52a8e6e2776c95b290a70e423ef461fa25ada45e
Eric <erictaynguyen@gmail.com>
2022-09-12 16:36:14 -0700

    using express method override

--- August/9-7/RESTDemo/index.js
+++ August/9-7/RESTDemo/index.js
@@ -2,12 +2,14 @@ const { text } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 const { v4: uuid } = require('uuid');
 
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
