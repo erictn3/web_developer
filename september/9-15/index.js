edbd7f46a5d0461123692748dbe997049726b978
Eric <erictaynguyen@gmail.com>
2022-09-15 11:13:22 -0700

    first mongoose model

--- september/9-15/index.js
+++ september/9-15/index.js
@@ -1,3 +1,28 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/movieApp');
+mongoose.connect('mongodb://localhost:27017/movieApp')
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+
+// {
+//     title: 'Amadeus',
+//         year: 1986,
+//             score: 9.2
+//     rating: 'R'
+// } 
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' })
