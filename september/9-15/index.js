0a6424674901453eb40a08b0b49c59517744cf7f
Eric <erictaynguyen@gmail.com>
2022-09-16 14:07:32 -0700

    mongoose find part 2

--- september/9-15/index.js
+++ september/9-15/index.js
@@ -27,14 +27,20 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model('Movie', movieSchema);
 // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' })
 
-Movie.insertMany([
-    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
-])
-    .then(data => {
-        console.log('it worked')
-        console.log(data)
-    })
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+// ])
+//     .then(data => {
+//         console.log('it worked')
+//         console.log(data)
+//     })
+
+// Movie.find({year: {$gt: 2010}}).then(data => console.log(data))
+// Movie.findOne({}).then(m => console.log(m))
+
+// Movie.find({_id: '3236aed9cd42e1a20b29aa7'}).then(m => console.log(m))
+// Movie.findById('3236aed9cd42e1a20b29aa7').then(m => console.log(m))
