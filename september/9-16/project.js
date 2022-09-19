a5a93c59c83600b28b2b6d4bbba7b0137dcfb56f
Eric <erictaynguyen@gmail.com>
2022-09-19 16:15:21 -0700

    additional schema constraingts part 3

--- september/9-16/project.js
+++ september/9-16/project.js
@@ -17,17 +17,33 @@ const productSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     onSale: {
         type: Boolean,
         default: false
+    },
+    categories: [String],
+    // categories: {
+    //     type: [String],
+    //     default: ['String']
+    // } 
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
     }
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: "Bike Helmet from helmet makers", price: 2000 });
+const bike = new Product({ name: "Bike Helmet", price: 2000, categories: ['cycling', 'safety'] });
 
 bike.save()
     .then(data => {
