40dae1a45608c3c176cf916c10bc91742bb82d87
Eric <erictaynguyen@gmail.com>
2022-09-21 14:42:00 -0700

    adding labels and other details to product create page

--- september/mongoose_express/index.js
+++ september/mongoose_express/index.js
@@ -18,6 +18,10 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
