493ad2cd40116568ac29b2d7f186e753e4b977fc
Eric <erictaynguyen@gmail.com>
2022-09-21 14:18:46 -0700

    added details page to products

--- september/mongoose_express/index.js
+++ september/mongoose_express/index.js
@@ -23,6 +23,13 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
 })
 
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product)
+    res.send('details page')
+})
+
 app.listen(3000, () => {
     console.log('APP IS LISTENING ON PORT 3000!')
 })
