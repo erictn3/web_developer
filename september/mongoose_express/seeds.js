8aac62b90a94884d03bf0a3b3732fdf4bbee1f45
Eric <erictaynguyen@gmail.com>
2022-09-21 13:46:15 -0700

    adding data to model

--- september/mongoose_express/seeds.js
+++ september/mongoose_express/seeds.js
@@ -1,8 +1,4 @@
-const express = require('express');
-const app = express();
-const path = require('path');
 const mongoose = require('mongoose');
-
 const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
@@ -14,14 +10,14 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
         console.log(err)
     })
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/dog', (req, res) => {
-    res.send('WOOF')
+const p = new Product({
+    name: 'Ruby Grapefruit',
+    price: 1.99,
+    category: 'fruit',
 })
-
-app.listen(3000, () => {
-    console.log('APP IS LISTENING ON PORT 3000!')
+p.save().then(p => {
+    console.log(p)
 })
+    .catch(e => {
+        console.log(e)
+    })
