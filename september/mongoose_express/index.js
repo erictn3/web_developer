b4190f8445f264f116763d3f856a51c559abd451
Eric <erictaynguyen@gmail.com>
2022-09-22 12:46:53 -0700

    filtering by category part 2

--- september/mongoose_express/index.js
+++ september/mongoose_express/index.js
@@ -38,10 +38,10 @@ app.get('/products', async (req, res) => {
     const { category } = req.query;
     if (category) {
         const products = await Product.find({ category })
-        res.render('products/index', { products })
+        res.render('products/index', { products, category })
     } else {
         const products = await Product.find({})
-        res.render('products/index', { products })
+        res.render('products/index', { products, category: 'All' })
     }
 
 
