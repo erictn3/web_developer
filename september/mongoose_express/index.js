a9a6d88aa860a4051dc3b4f4147f10d55a1d009e
Eric <erictaynguyen@gmail.com>
2022-09-22 12:23:39 -0700

    filtering by category

--- september/mongoose_express/index.js
+++ september/mongoose_express/index.js
@@ -35,8 +35,16 @@ app.post('/products', async (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products })
+    }
+
+
 })
 
 app.get('/products/:id', async (req, res) => {
