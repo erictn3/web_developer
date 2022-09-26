881086596f7e5b2411529477d2f4ab77399fe979
Eric <erictaynguyen@gmail.com>
2022-09-26 16:22:42 -0700

    part 2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -30,7 +30,8 @@ app.get('/campgrounds', async (req, res) => {
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
-    res.render('campgrounds/show');
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
 })
 
 // app.get('/makecampground', async (req, res) => {
