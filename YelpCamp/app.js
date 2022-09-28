81905e64c05294301bf24e8225b2720caf11f206
Eric <erictaynguyen@gmail.com>
2022-09-28 15:39:29 -0700

    method override for put/patch

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const Campground = require('./models/campground')
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     // useNewUrlParser: true,
@@ -21,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -33,6 +36,10 @@ app.get('/campgrounds', async (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    res.send('it worked')
 })
 
 app.post('/campgrounds', async (req, res) => {
