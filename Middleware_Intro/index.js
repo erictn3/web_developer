6165031f735777eedbccb885a89d0c4de7e644b5
Eric <erictaynguyen@gmail.com>
2022-09-29 14:02:08 -0700

    middleware intro

---
+++ Middleware_Intro/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF WOOF!')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost: 3000')
+})
