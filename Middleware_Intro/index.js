8fce8d69e436281ec9e39c01ea65a0f8b5ca9731
Eric <erictaynguyen@gmail.com>
2022-09-30 16:06:07 -0700

    more middleware practice

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'))
+app.use(morgan('tiny'))
+
+app.use((req, res, next) => {
+    console.log(req.method.toUpperCase(), req.path)
+    next();
+})
 
 // this function works on every single request
-app.use((req, res, next) => {
-    console.log("THIS IS MY FIRST MIDDLEWARE")
-    next();
-})
-app.use((req, res, next) => {
-    console.log("THIS IS MY SECOND MIDDLEWARE")
-    next();
-})
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE")
+
+//     next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+
+// })
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE")
+//     next();
+// })
 
 app.get('/', (req, res) => {
     res.send('Home Page')
