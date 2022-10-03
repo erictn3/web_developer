f69acc8a0e68b270e287eb3b84e0e30b63225219
Eric <erictaynguyen@gmail.com>
2022-10-03 16:01:36 -0700

    middleware practice: order matters

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 app.use(morgan('tiny'))
 
 app.use((req, res, next) => {
-    console.log(req.method.toUpperCase(), req.path)
+    req.requestTime = Date.now();
+    console.log(req.method, req.path)
     next();
 })
 
@@ -24,10 +25,12 @@ app.use((req, res, next) => {
 // })
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('Home Page')
 })
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('WOOF WOOF!')
 })
 
