080e7715b957b19635b0e2cf6293b75e6348971e
Eric <erictaynguyen@gmail.com>
2022-10-06 14:01:11 -0700

    protecting specific routes

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -47,6 +47,9 @@ app.get('/dogs', (req, res) => {
     res.send('WOOF WOOF!')
 })
 
+app.get('/secret', (req, res) => {
+    res.send('my secret is: sometimes i wear headphones in public so I dont talk to anyone')
+})
 
 
 app.use((req, res) => {
