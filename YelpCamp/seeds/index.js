b1e1ab34e09057052ed6f3864522f267e692ec20
Eric <erictaynguyen@gmail.com>
2022-10-11 16:58:18 -0700

    adding image

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await c.save();
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1436712702142-096ca31693c4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY2NTUzMjQ2Nw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+            description: "this is the description my good sir this is the description my good sirthis is the description my good sirthis is the description my good sir",
+            price
         })
         await camp.save();
     }
@@ -30,3 +34,4 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
+
