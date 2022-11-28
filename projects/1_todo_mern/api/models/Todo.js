fc7400a0cf90bcf5f2237e897de192ca123f128d
Eric <erictaynguyen@gmail.com>
2022-11-28 13:08:13 -0800

    model & Todojs

---
+++ projects/1_todo_mern/api/models/Todo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+    }
+})
--- projects/1_todo_mern/api/server.js
+++ projects/1_todo_mern/api/server.js
@@ -15,10 +15,18 @@ app.use(express.json());
 app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    userUnifiedTopology: true
-})
-    .then(() => console.log("Connected to DB"))
-    .catch(console.error);
+    // useNewUrlParser: true,
+    // userUnifiedTopology: true
 
-app.listen(3001, () => console.log("Server Started on pport 3001"));
+})
+// .then(() => console.log("Connected to DB"))
+// .catch(console.error);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+app.listen(3001, () => console.log("Server Started on port 3001"));
