f8f3b108d3a1946049cb782dc1872c559e8f9233
Eric <erictaynguyen@gmail.com>
2022-12-06 16:51:11 -0800

    useState

--- projects/1_todo_mern/client/src/App.js
+++ projects/1_todo_mern/client/src/App.js
@@ -1,26 +1,39 @@
 import { useState, useEffect } from 'react';
 
+const API_BASE = "http://localhost:3000";
+
 function App() {
 
   const [todos, setTodos] = useState([]);
   const [popupActive, setPopupActive] = useState(false);
-  const [newTodo,]
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+    console.log(todos)
+  }, [])
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then(res => res.json())
+      .then(data => setTodos(data))
+      .catch(err => console.error("Error:", err));
+  }
 
   return (
     <div className="App">
       <h1>Welcome, Eric</h1>
       <h4>Your Tasks</h4>
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
-          <div className="text">Get the bread</div>
-          <div class="delete-todo">x</div>
-        </div>
-        <div className="todo is-complete">
-          <div className="checkbox"></div>
-          <div className="text">Get the milk</div>
-          <div class="delete-todo">x</div>
-        </div>
+        {todos.map(todo => (
+
+          <div className="todo">
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div class="delete-todo">x</div>
+          </div>
+        ))}
+
 
       </div>
     </div>
