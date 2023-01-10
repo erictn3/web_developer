05d4a2fc8845b660abd736639032ef6de94cdb8e
Eric <erictaynguyen@gmail.com>
2023-01-09 16:21:10 -0800

    markup for calculator

---
+++ projects/4_calc/calculator/src/App.js
@@ -0,0 +1,41 @@
+function App() {
+
+	const createDigits = () => {
+		const digits = [];
+
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				// why do I need key here?
+				<button key={i}>{i}</button>
+			)
+		}
+		return digits;
+	}
+
+	return (
+		<div className="App">
+			<div class="calculator">
+				<div class="display">
+					<span>(0)</span> 0
+				</div>
+				<div class="operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
+
+					<button>DEL</button>
+				</div>
+
+				<div class="digits">
+					{createDigits()}
+					<button>0</button>
+					<button>.</button>
+					<button>=</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
